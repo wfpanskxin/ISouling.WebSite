9a5bccc32ff670373ae34497035e146469819802
彭伟 <pengweiqhca@sina.com>
2017-05-11 21:14:29 +0800

    TPT

--- src/ISoul.WebSite/ISoul.WebSite.Www/Data/UserDbContext.cs
+++ src/ISoul.WebSite/ISoul.WebSite.Www/Data/UserDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using ISoul.Component.User;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ISoul.WebSite.Www.Data
@@ -16,6 +17,9 @@ namespace ISoul.WebSite.Www.Data
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
+
+            builder.Entity<MemberUser>();
+            builder.Entity<CompanyUser>();
             builder.Entity<IdentityUser<int>>().ToTable("tbl_Users");
         }
     }
