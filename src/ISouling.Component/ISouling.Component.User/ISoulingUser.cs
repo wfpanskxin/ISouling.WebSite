d47bde70b84de1b958cb1bddb5daea39db854cfe
彭伟 <pengweiqhca@sina.com>
2017-05-23 20:58:27 +0800

    renmae ISoul => ISouling

---
+++ src/ISouling.Component/ISouling.Component.User/ISoulingUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace ISouling.Component.User
+{
+    // Add profile data for application users by adding properties to the MemberUser class
+    public class MemberUser : IdentityUser<int>
+    {
+    }
+
+    // Add profile data for application users by adding properties to the CompanyUser class
+    public class CompanyUser : IdentityUser<int>
+    {
+    }
+}
