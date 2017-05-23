a87d4d0abb7dce2994ae0efa039580dae83c7838
彭伟 <pengweiqhca@sina.com>
2017-05-23 20:37:50 +0800

    添加第一个页面

---
+++ src/ISoul.WebSite/ISoul.WebSite.Www/Controllers/MemberController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ISoul.WebSite.Www.Controllers
+{
+    public class MemberController : Controller
+    {
+        public IActionResult Personality()
+        {
+            return View();
+        }
+    }
+}
